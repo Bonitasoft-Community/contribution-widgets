bbac9f1505c1a150a95257837115fa365958ea7e
Pablo Alonso <pablo.alonso@bonitasoft.com>
2017-03-23 16:41:45 +0100

    First Version of Multiselect Widget

---
+++ src/widgets/customMultiSelect/test/widget.spec.js
@@ -0,0 +1,32 @@
+describe('Widget', function () {
+
+  var $compile, scope;
+
+  // add both, module required by the widget and locally defined ones
+  beforeEach(function () {
+    jasmine.getJSONFixtures().fixturesPath = 'base/src/widgets/customMultiSelect/';
+    loadJSONFixtures('widget.json');
+    
+    (getJSONFixture('widget.json').requiredModules || []).concat([
+      'bonitasoft.ui.widgets',
+      'bonitasoft.ui.filters'
+    ]).forEach(function (module) {
+      angular.mock.module(module);
+    });
+  });
+
+  beforeEach(inject(function (_$compile_, $rootScope) {
+    $compile = _$compile_;
+    scope = $rootScope.$new();
+    scope.properties = {};
+  }));
+
+  it('should contains specified html', function () {
+    scope.properties.value = 'Hello';
+
+    var element = $compile('<custom-widget></custom-widget>')(scope);
+    scope.$apply();
+
+    //expect(element.text().trim()).toBe('Value is: Hello. Click me to toggle background color');
+  });
+});
