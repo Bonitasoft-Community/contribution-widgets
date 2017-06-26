a8c7629ea841f7643eef880c4b5b974c7a5ff165
lionel-bonitasoft <lionel.palacin@bonitasoft.com>
2017-06-26 16:27:44 -0400

    Add deploy all option on build command.

--- gulpfile.js
+++ gulpfile.js
@@ -13,19 +13,26 @@ var runKarma = require('./script/run-karma');
 var dir = require('./script/dir');
 var deploy = require('./script/deploy');
 
-gulp.task('clean', function () {
+gulp.task('clean', function() {
   return gulp.src('dist').pipe(clean());
 });
 
-gulp.task('build', function () {
-  return merge(dir('src/widgets').map(function (widget) {
+gulp.task('build', function() {
+  return merge(dir('src/widgets').map(function(widget) {
     return merge(
-      gulp.src(join('src/widgets', widget, '*.json')).pipe(buildWidget()),
-      gulp.src(join('src/widgets', widget, 'assets/**/*')).pipe(rename(function (path) {
-        path.dirname = join(widget, 'assets', path.dirname);
-      })))
+        gulp.src(join('src/widgets', widget, '*.json')).pipe(buildWidget()),
+        gulp.src(join('src/widgets', widget, 'assets/**/*')).pipe(rename(function(path) {
+          path.dirname = join(widget, 'assets', path.dirname);
+        })))
       .pipe(gulp.dest(join('dist', widget, 'resources')));
   }));
+});
+
+gulp.task('deployAll', ['zip'], function() {
+  var argv = getDeploymentAllArguments();
+  return dir('dist').map(function(widget) {
+    return gulp.src(join('dist', widget + '.zip')).pipe(deploy(argv.host, argv.force));
+  });
 });
 
 gulp.task('test', ['build'], function(done) {
@@ -34,24 +41,26 @@ gulp.task('test', ['build'], function(done) {
     gulp.watch('src/widgets/**/(!*.spec.js)', ['build']);
   }
   Promise
-    .all(dir('src/widgets').map(runKarma.bind(null, {watch})))
+    .all(dir('src/widgets').map(runKarma.bind(null, {
+      watch
+    })))
     .then(done.bind(null, null));
 });
 
-gulp.task('zip', ['build'], function () {
-  return merge(dir('dist').map(function (widget) {
+gulp.task('zip', ['build'], function() {
+  return merge(dir('dist').map(function(widget) {
     return gulp.src(join('dist', widget, '**/*'))
       .pipe(zip(widget + '.zip'))
       .pipe(gulp.dest('dist'));
   }));
 });
 
-gulp.task('deploy:single', ['zip'], function () {
+gulp.task('deploy:single', ['zip'], function() {
   var argv = getDeploymentArguments();
   return gulp.src(join('dist', argv.widget + '.zip')).pipe(deploy(argv.host, argv.force));
 });
 
-gulp.task('deploy:watch', ['deploy:single'], function () {
+gulp.task('deploy:watch', ['deploy:single'], function() {
   var argv = getDeploymentArguments();
   return gulp.watch(join('src/widgets', argv.widget, '**/*'), ['deploy:single']);
 });
@@ -65,6 +74,7 @@ gulp.task('default', ['clean'], function() {
 });
 
 function getDeploymentArguments() {
+  //console.log(sing)
   return yargs
     .usage('Usage: npm run deploy -- --widget customWidget [--host http://127.0.0.1:8080/designer] [--force] [--watch]')
 
@@ -86,6 +96,24 @@ function getDeploymentArguments() {
     .demand('watch', false)
     .default('watch', false)
     .describe('watch', 'Watch source files and redeploy widget whenever a source file changes')
+
+    .argv;
+}
+
+function getDeploymentAllArguments(singleWidget) {
+
+  return yargs
+    .usage('Usage: npm run deployAll -- [--host http://127.0.0.1:8080/designer] [--force] ')
+
+    .demand('host', false)
+    .alias('host', 'h')
+    .default('host', 'http://127.0.0.1:8080/designer')
+    .describe('host', 'UI Designer URL')
+
+    .demand('force', false)
+    .alias('force', 'f')
+    .default('force', false)
+    .describe('force', 'Override the widget if it already exist at destination')
 
     .argv;
 }
